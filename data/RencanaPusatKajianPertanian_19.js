917716b63d237c4d234a8236fa22a8d6b9878153
StudioBayat <58888409+StudioBayat@users.noreply.github.com>
2019-12-15 01:44:34 +0700

    Add files via upload

---
+++ data/RencanaPusatKajianPertanian_19.js
@@ -0,0 +1,8 @@
+var json_RencanaPusatKajianPertanian_19 = {
+"type": "FeatureCollection",
+"name": "RencanaPusatKajianPertanian_19",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pusat Kajian Pertanian" }, "geometry": { "type": "Point", "coordinates": [ 110.652240301000063, -7.783614655999941 ] } }
+]
+}
